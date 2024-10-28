f943f2b669f024ad3d80d529c99b138f07ff8372
Timur Munykin <timur.munykin@gmail.com>
2024-10-29 00:33:08 +0100

    update run prod sh with multiversion of docker sompose

--- run.prod.sh
+++ run.prod.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
+
 echo "Starting production environment with Docker Compose..."
-docker-compose up --build -d
+
+# Check if docker-compose or docker compose is available
+if command -v docker-compose &> /dev/null
+then
+    docker-compose up --build -d
+elif docker compose version &> /dev/null
+then
+    docker compose up --build -d
+else
+    echo "Neither docker-compose nor docker compose is available. Please install Docker Compose."
+    exit 1
+fi
